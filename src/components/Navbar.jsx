d3dac8d93668eb02f3e719a6f1e59f34d0997cf4
Akshay Kokatey <jitcse.akshay@gmail.com>
2024-03-07 23:51:09 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,143 @@
+// import { useState } from "react";
+
+// import { close, logo, menu } from "../assets";
+// import { navLinks } from "../constants";
+
+// const Navbar = () => {
+//   const [active, setActive] = useState("Home");
+//   const [toggle, setToggle] = useState(false);
+
+//   return (
+//     <nav className="w-full flex py-6 justify-between items-center navbar">
+//       <img src={logo} alt="Academic Vista"  />
+
+//       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+//         {navLinks.map((nav, index) => (
+//           <li
+//             key={nav.id}
+//             className={`font-poppins font-normal cursor-pointer text-[16px] ${
+//               active === nav.title ? "text-white" : "text-dimWhite"
+//             } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+//             onClick={() => setActive(nav.title)}
+//           >
+//             <a href={`#${nav.id}`}>{nav.title}</a>
+//           </li>
+//         ))}
+//       </ul>
+
+//       <div className="sm:hidden flex flex-1 justify-end items-center">
+//         <img
+//           src={toggle ? close : menu}
+//           alt="menu"
+//           className="w-[28px] h-[28px] object-contain"
+//           onClick={() => setToggle(!toggle)}
+//         />
+
+//         <div
+//           className={`${
+//             !toggle ? "hidden" : "flex"
+//           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+//         >
+//           <ul className="list-none flex justify-end items-start flex-1 flex-col">
+//             {navLinks.map((nav, index) => (
+//               <li
+//                 key={nav.id}
+//                 className={`font-poppins font-medium cursor-pointer text-[16px] ${
+//                   active === nav.title ? "text-white" : "text-dimWhite"
+//                 } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+//                 onClick={() => setActive(nav.title)}
+//               >
+//                 <a href={`#${nav.id}`}>{nav.title}</a>
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+// Navbar.js
+import { close, logo, menu } from "../assets";
+import React, { useState } from 'react';
+import ContactUsPopup from './ContactUsPopup';
+import { navLinks } from '../constants'; // Import your navigation links
+
+const Navbar = () => {
+  const [active, setActive] = useState('Home');
+  const [toggle, setToggle] = useState(false);
+  const [showContactUsPopup, setShowContactUsPopup] = useState(false);
+
+  const openContactUsPopup = () => {
+    setShowContactUsPopup(true);
+  };
+
+  const closeContactUsPopup = () => {
+    setShowContactUsPopup(false);
+  };
+
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="Academic Vista" />
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              active === nav.title ? 'text-white' : 'text-dimWhite'
+            } ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}
+            onClick={() => {
+              setActive(nav.title);
+              if (nav.id === 'contact') {
+                openContactUsPopup();
+              }
+            }}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(!toggle)}
+        />
+
+        <div
+          className={`${
+            !toggle ? 'hidden' : 'flex'
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? 'text-white' : 'text-dimWhite'
+                } ${index === navLinks.length - 1 ? 'mb-0' : 'mb-4'}`}
+                onClick={() => {
+                  setActive(nav.title);
+                  if (nav.id === 'contact') {
+                    openContactUsPopup();
+                  }
+                }}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      {showContactUsPopup && <ContactUsPopup onClose={closeContactUsPopup} />}
+    </nav>
+  );
+};
+
+export default Navbar;
